f21965dc01ae358670efd7105d1592cb3fc5348d
unknown <ahmednahid1995@gmail.com>
2022-03-01 21:31:19 +0600

    style and more validation

--- js/phone.js
+++ js/phone.js
@@ -1,34 +1,52 @@
-document.getElementById('notFound').style.display = 'none';
+document.getElementById('noName').style.display = 'none';
+// load all data
 const loadPhone = () => {
     const inputField = document.getElementById('input-field');
     const inputText = inputField.value;
 
     // clear input field
     inputField.value = '';
+
     if (inputText == '') {
-        document.getElementById('notFound').style.display = 'block';
+        // msg for empty input
+        document.getElementById('noName').style.display = 'block';
+        document.getElementById('notFound').style.display = 'none';
 
         const displayDiv = document.getElementById('search-result');
         // clear previous search result
         displayDiv.textContent = '';
     }
     else {
-        document.getElementById('notFound').style.display = 'none';
+        document.getElementById('noName').style.display = 'none';
 
+        // fetct data
         fetch(`https://openapi.programming-hero.com/api/phones?search=${inputText}`)
             .then(res => res.json())
-            .then(data => displayPhone(data.data))
+            .then(data => displayPhone(data))
     }
 }
 
+document.getElementById('notFound').style.display = 'none';
+
 const displayPhone = phones => {
+    // get the array of phone
+    const allPhones = phones.data;
+
     const displayDiv = document.getElementById('search-result');
     // clear previous search result
     displayDiv.textContent = '';
 
-    phones.forEach(phone => {
-        const div = document.createElement('div');
-        div.innerHTML = `
+    if (phones.status === false) {
+        // msg for not found the phone
+        document.getElementById('notFound').style.display = 'block';
+        document.getElementById('noName').style.display = 'none';
+    }
+    else {
+        document.getElementById('notFound').style.display = 'none';
+        allPhones.forEach(phone => {
+            const div = document.createElement('div');
+            // dynamic html
+            div.innerHTML = `
                             <div class="col">
                                 <div class="card">
                                 <img class="w-50 mx-auto mt-3" src="${phone.image}" class="card-img-top" alt="...">
@@ -39,6 +57,7 @@ const displayPhone = phones => {
                             </div>
                             </div>
                         </div>`;
-        displayDiv.appendChild(div);
-    })
+            displayDiv.appendChild(div);
+        })
+    }
 }
